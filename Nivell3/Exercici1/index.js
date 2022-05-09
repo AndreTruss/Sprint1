806b396cca4d2a0239ad31dec4fea1bfb265c4bc
Andrea Trussardi <andreatrussardi@imac-di-andrea-trussardi.local>
2022-05-09 21:16:12 +0200

    Correct nivell3 ex1 sprint1

--- Nivell3/Exercici1/index.js
+++ Nivell3/Exercici1/index.js
@@ -1,16 +1,7 @@
-const arrayFunctions = [];
+const arrayFunctions = []
 
-const countNumber = () => {
-    const array = []
-    for ( let i = 0; i < 10; i++ ) { 
-        array.push (i) 
-    }
-    return array
-};
+const countNumber = () => { for ( let i = 0; i < 10; i++ ) console.log( i ) }
+    
+for ( let i = 0; i < 10; i++ ) arrayFunctions.push( countNumber ) 
 
-for ( let j = 0; j < 10; j++ ) { 
-    arrayFunctions.push ( countNumber() ) 
-};
-
-arrayFunctions.forEach ( element => console.log ( element ) )
-
+for (let funct of arrayFunctions) funct()  
