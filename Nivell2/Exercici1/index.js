559d0608b6b024f7a87763780447f4916b2efee5
Andrea Trussardi <andreatrussardi@imac-di-andrea-trussardi.local>
2022-05-09 14:28:52 +0200

    Nivell 2 exercicis fets

---
+++ Nivell2/Exercici1/index.js
@@ -0,0 +1,8 @@
+const nome = 'Andrea'
+const cognome = 'Trussardi'
+
+function giveName(name, surname) {
+    console.log(`My name is ${name} ${surname}`)
+}
+
+giveName(nome, cognome)
---
+++ Nivell2/Exercici2/index.js
@@ -0,0 +1,8 @@
+const nome = 'Andrea'
+const cognome = 'Trussardi'
+
+function giveName(name, surname) {
+    return `My name is ${name} ${surname}`
+}
+
+console.log( `${ giveName(nome, cognome) }` )
