e7fa52021568d8bf74a0b508f7c7d4f8ab7766ef
Andrea Trussardi <andreatrussardi@imac-di-andrea-trussardi.local>
2022-05-12 16:47:00 +0200

    Working on Entrega1_3

--- Entrega1_3.js
+++ Entrega1_3.js
@@ -1,19 +1,26 @@
 // Nivell1 Exercici1
+//Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep. 
+//Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent 
+//depenent de si la Promise es resol o no.
 
-let myPromise = () => { return new Promise( (resolve, reject) => {
-	setTimeout( () => resolve('¡Right!'), 250)
-	setTimeout( () => reject('¡Wrong!'), 280)
-}) 
+let myPromise = () => { 
+	return new Promise( (resolve, reject) => {
+		setTimeout( () => resolve('¡Right!'), 250)
+		setTimeout( () => reject( new Error( '¡Wrong!' ) ), 280)
+	}) 
 }
-const successCallback = (value) =>  console.log(`¡Yes! ${value}`) 
-const failureCallback = (err) => console.log(err.message)
+const successCallback = value => console.log( `¡Yes! ${value}` ) 
+const failureCallback = error => console.log( `¡No! ${error.message}` )
 
 myPromise()
-.then(successCallback)
-.catch(failureCallback) 
+	.then( successCallback )
+	.catch( failureCallback ) 
 
 
 // Nivell1 Exercici2
+//Crea una arrow function que rebi un paràmetre i una funció callback 
+//i li passi a la funció un missatge o un altre 
+//(que s'imprimirà per consola) en funció del paràmetre rebut.
 
 const giveMessage = ( message ) => console.log( message )
 
@@ -24,6 +31,8 @@ myFunction('Hola Andrea', giveMessage)
     
 
 // Nivell2 Exercici1
+//Donats els objectes employees i salaries, crea una arrow function getEmployee() 
+//que retorni una Promise efectuant la cerca en l'objecte pel seu id.
 
 let employees = [{
     id: 1,
@@ -47,58 +56,60 @@ let salaries = [{
     salary: 2000
 }];
 
-const getEmployees = empId => new Promise( (resolve, reject) => { 
-	let worker = employees.find( element => element.id == empId)
-	//console.log(worker) 
-	 
-	worker 
-	? resolve(`Found employee: ${worker.name}`)
-	: reject(new Error('¡Failure!') )}
+const getEmployee = empId => 
+	new Promise( (resolve, reject) => { 
+		let worker = employees.find( element => element.id == empId)
+	
+		worker 
+			? resolve(worker)
+			: reject(new Error('¡Failure!') )
+	}
 ) 
 
-getEmployees(2)
-.then(successCallback)
-.catch(failureCallback)
+getEmployee(2)
+	.then(value => console.log(`Found employee: ${value.name}`))
 	
 
 // Nivell2 Exercici2
+// Crea una altra arrow function getSalary que rebi com a paràmetre un objecte employee 
+//i retorni el seu salari.
 
-const getSalary = (empObj) => new Promise( (resolve, reject) => { 
-	let index = employees.findIndex( indice => indice == empObj)
-	//console.log(index) 
+const getSalary = employee => 
+	new Promise( (resolve, reject) => { 
+		let wage = salaries.find( element => element.id == employee.id)
 	 
-	index !== -1 
-	? resolve(`Found salary: ${salaries[index].salary}`)
-	: reject(new Error('¡Failure!') )}
+		wage  
+			? resolve(wage)
+			: reject(new Error('¡Failure!') )
+	}
 ) 
 
-getSalary(employees[2])
-.then(successCallback)
-.catch(failureCallback)
+getSalary(employees[1])
+	.then(value => console.log(`Found salary: ${value.salary}`))
+
 
 // Nivell2 Exercici3
+// Invoca la primera funció getEmployee i després getSalary niant l'execució de les dues promises.
 
-const getEmploy = empId => new Promise( (resolve, reject) => { 
-	let worker = employees.find( element => element.id == empId)
-	//console.log(worker) 	 
-	worker
-	? resolve(worker)
-    : reject(new Error('¡Failure!') )
-}) 
+getEmployee(1)
+	.then( valueEmp => 
+		getSalary(valueEmp)
+		.then( valueSal => 
+			console.log(`Employee: ${valueEmp.name} have salary: ${valueSal.salary}`)
+		)
+	)
 
-const anidarFunction = worker => {
-    let index = employees.findIndex( indice => indice == worker)
-    const messaggio = `Employee: ${worker.name} have salary: ${salaries[index].salary}`
-    return Promise.resolve(messaggio)
-}
-getEmploy(2)
-.then(anidarFunction)
-.then(successCallback)
-.catch(failureCallback)
 
 //Nivell3 Exercici1
+// Fixa un element catch a la invocació del nivell anterior 
+//que capturi qualsevol error i el mostri per la consola.
 
-getEmploy(5)
-.then(anidarFunction)
-.then(successCallback)
-.catch(failureCallback)
+getEmployee(5)
+	.then( valueEmp => 
+		getSalary(valueEmp)
+		.then( valueSal => 
+			console.log(`Employee: ${valueEmp.name} have salary: ${valueSal.salary}`)
+		)
+		.catch(failureCallback)
+	)
+	.catch(failureCallback)
