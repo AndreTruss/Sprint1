bb6e56c64cb743b21c7ebe742452163e3f684dd1
Andrea Trussardi <andreatrussardi@imac-di-andrea-trussardi.local>
2022-05-14 12:17:31 +0200

    working on 1.5stage

--- Entrega1_5Node.js
+++ Entrega1_5Node.js
@@ -74,4 +74,49 @@ async function recursiveFunction( ms ) {
         console.log( err.message )
     }
 }
-recursiveFunction( 1000 )
+//recursiveFunction( 1000 )
+
+// Nivell2 Exercici2
+// Crea una funció que llisti per la consola el contingut 
+// del directori d'usuari de l'ordinador utilizant Node Child Processes.
+
+
+const { spawn } = require('child_process');
+const os = require('os');
+
+const userDirectory = os.homedir()
+const ls = spawn('ls', [ userDirectory ]);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+
+/*fs.readdir(directory, function (err, files) {
+    
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    
+    files.forEach(function (file) {
+        
+        console.log(file); 
+    });
+}); */
+
+// Nivell3 Exercici1
+// Crea una funció que creï dos fitxers codificats en hexadecimal i en base64 respectivament, 
+// a partir del fitxer del nivell 1.
+// Crea una funció que guardi els fitxers del punt anterior, 
+// ara encriptats amb l'algoritme aes-192-cbc, i esborri els fitxers inicials.
+// Crea una altra funció que desencripti i descodifiqui els fitxers de l'apartat anterior 
+// tornant a generar una còpia de l'inicial.
+// Inclou un README amb instruccions per a l'execució de cada part.
