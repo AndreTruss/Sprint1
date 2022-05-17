888478983ed6c1d416e5d17a553f39b21ab5c08b
Andrea Trussardi <andrea@>
2022-05-17 23:19:03 +0200

    Testing fail 3 of 1

--- Entrega1_6Test/__test__/index.test.js
+++ Entrega1_6Test/__test__/index.test.js
@@ -3,16 +3,16 @@ const rest = require('../app/index.js')
 const multiply = require('../app/index.js')
 const divide = require('../app/index.js')
 
-test('adds 1 + 2 + 3 to equal 6', () => {
-  expect(sum(1, 2, 3)).toBe(6)
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5)
 })
 
-test('rests 10 - 2 - 3 to equal 5', () => {
-    expect(rest(10, 2, 3)).toBe(5)
+test('rests 10 - 2 to equal 8', () => {
+    expect(rest(10, 2)).toBe(8)
 })
 
-test('multyplies 10 * 2 * 3 to equal 60', () => {
-  expect(multiply(10, 2, 3)).toBe(60)
+test('multyplies 10 * 2 to equal 20', () => {
+  expect(multiply(10, 2)).toBe(20)
 })
 
 test('divides 6 / 2 to equal 3', () => {
--- Entrega1_6Test/app/index.js
+++ Entrega1_6Test/app/index.js
@@ -1,8 +1,8 @@
 // function sum, rest, multiply and divide
 
-const sum = (a, b, c ) => { return (a + b + c )}
-const rest = (a, b, c ) => { return a - b - c }
-const multiply = (a, b, c ) => { return (a * b * c) }
+function sum(a, b) { return (a + b) }
+const rest = (a, b ) => { return a - b }
+const multiply = (a, b ) => { return (a * b ) }
 const divide = (a, b ) => { return a / b }
 
 module.exports = sum
