a25f0cecee573a0625ffceb46dde27da9cff8807
Andrea Trussardi <andrea@>
2022-05-17 20:24:34 +0200

    Starting tests

---
+++ Entrega1_6Test/__test__/index.test.js
@@ -0,0 +1,20 @@
+const sum = require('../app/index.js')
+const rest = require('../app/index.js')
+const multiply = require('../app/index.js')
+const divide = require('../app/index.js')
+
+test('adds 1 + 2 + 3 to equal 6', () => {
+  expect(sum(1, 2, 3)).toBe(6)
+})
+
+test('rests 10 - 2 - 3 to equal 5', () => {
+    expect(rest(10, 2, 3)).toBe(5)
+})
+
+test('multyplies 10 * 2 * 3 to equal 60', () => {
+  expect(multiply(10, 2, 3)).toBe(60)
+})
+
+test('divides 6 / 2 to equal 3', () => {
+    expect(divide(6, 2)).toBe(3)
+})
---
+++ Entrega1_6Test/app/index.js
@@ -0,0 +1,11 @@
+// function sum, rest, multiply and divide
+
+const sum = (a, b, c ) => { return (a + b + c )}
+const rest = (a, b, c ) => { return a - b - c }
+const multiply = (a, b, c ) => { return (a * b * c) }
+const divide = (a, b ) => { return a / b }
+
+module.exports = sum
+module.exports = rest
+module.exports = multiply
+module.exports = divide
