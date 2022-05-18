b903717f83d4fdf46af93304c3993d7e4573d8b4
unknown <formacio@PC-1062619.formacio.local>
2022-05-18 12:49:23 +0200

    Working on test

--- Entrega1_6Test/__test__/operation.test.js
+++ Entrega1_6Test/__test__/operation.test.js
@@ -1,4 +1,6 @@
-const { sum, rest, multiply, divide } = require('../app/operation.js')
+//Crea un arxiu amb les funcions sumar, restar, multiplicar i dividir dos o més operands. Testeja la correcta execució d'aquestes funcions.
+
+const { sum, rest, multiply, divide, functEmployees } = require('../app/operation.js')
 
 test('adds 1 + 2 + 3 to equal 6', () => {
   expect( sum( 1, 2, 3 ) ).toBe(6)
@@ -15,3 +17,22 @@ test('multyplies 10 * 2 * 3 to equal 60', () => {
 test('divides 6 / 2 to equal 3', () => {
     expect( divide( 6, 2 ) ).toBe(3)
 })
+
+
+
+test('add id employees 1 to equal Linux Torvalds', async () => {
+
+  return expect(functEmployees(1)).resolves.toBe('Employee: Linux Torvalds, have salary: 4000');
+});
+
+/* test('el fecth falla con un error', async () => {
+  await expect(functEmployees(5)).rejects.toMatch('error');
+}); */
+test('the fetch fails with an error', async () => {
+  
+  try {
+    await functEmployees(1);
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+});
--- Entrega1_6Test/app/operation.js
+++ Entrega1_6Test/app/operation.js
@@ -5,10 +5,64 @@ const rest = ( a, b, c ) => { return (a - b - c ) }
 const multiply = ( a, b, c ) => { return (a * b * c ) }
 const divide = ( a, b ) => { return (a / b ) }
 
+
+
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+const getEmployee = empId => 
+	new Promise( (resolve, reject) => { 
+		let worker = employees.find( element => element.id == empId)
+	
+		worker 
+			? resolve(worker)
+			: reject(new Error('¡Fail exercici 1.1!') )
+	}
+) 
+
+const getSalary = employee => 
+	new Promise( (resolve, reject) => { 
+		let wage = salaries.find( element => element.id == employee.id)
+	 
+		wage  
+			? resolve(`Employee: ${employee.name}, have salary: ${wage.salary}`)
+			: reject(new Error('¡Fail ecercici 1.1!') )
+	}
+) 
+
+async function functEmployees(empId){
+    try {
+        const employee = await getEmployee(empId)
+        const message = await getSalary(employee)
+        console.log (message)
+    } catch (error) {
+        console.log(`¡No! ${error.message}`) 
+    }
+}
+functEmployees(1)
 module.exports = {
     sum,
     rest,
     multiply,
-    divide
+    divide,
+    functEmployees
 }
-
