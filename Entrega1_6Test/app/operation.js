885bda3eca80db445026dcbc01b7c79cd41c792b
unknown <formacio@PC-1062619.formacio.local>
2022-05-18 10:10:53 +0200

    first test works

---
+++ Entrega1_6Test/app/operation.js
@@ -0,0 +1,14 @@
+// function sum, rest, multiply and divide
+
+const sum = ( a, b, c ) => { return (a + b + c ) }
+const rest = ( a, b, c ) => { return (a - b - c ) }
+const multiply = ( a, b, c ) => { return (a * b * c ) }
+const divide = ( a, b ) => { return (a / b ) }
+
+module.exports = {
+    sum,
+    rest,
+    multiply,
+    divide
+}
+
